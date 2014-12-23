1690dbfae627d10ec884249dff7622415a3fcd11
rohit01 <rohit.kgec@gmail.com>
2014-12-23 12:38:26 +0530

    Report index page added

--- umbrella/views/report.py
+++ umbrella/views/report.py
@@ -2,7 +2,7 @@
 #
 
 from umbrella.model.report import TagReport
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request
 
 
 mod = Blueprint('report', __name__, url_prefix='/report')
@@ -13,16 +13,19 @@ DEFAULT_REPORT = 'Owner'
 def index():
     reports = TagReport()
     tag_keys = reports.get_tags_info().keys()
-    if DEFAULT_REPORT in tag_keys:
+    redirect_default = request.args.get('redirect', 'true').lower() == 'true'
+    if (DEFAULT_REPORT in tag_keys) and redirect_default:
         return redirect(url_for('report.report', tag_name=DEFAULT_REPORT))
     else:
-        return render_template('report/index.html')
+        return render_template('report/index.html', tag_keys=tag_keys)
 
 
 @mod.route("/<tag_name>")
 def report(tag_name):
     reports = TagReport()
     tag_keys = reports.get_tags_info().keys()
+    if tag_name not in tag_keys:
+        return redirect(url_for('report.index', redirect=False))
     tag_resources = reports.get_tag_resources(tag_name)
     return render_template(
         'report/report.html',
